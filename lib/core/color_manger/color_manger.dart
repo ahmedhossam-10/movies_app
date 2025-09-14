6d609aafb7807f8a83bc38b392d171b1fdfb9ec4
ahmedhossam-10 <ahmedhossam.4940@gmail.com>
2025-09-14 04:42:03 +0300

    +add assets (images-svg) +add color_manger file

---
+++ lib/core/color_manger/color_manger.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+abstract class ColorManger{
+  static const Color White =Color(0xFFFFFFFF);
+  static const Color Gray =Color(0xFFADADAD);
+  static const Color Red =Color(0xFFE82626);
+  static const Color DarkGray =Color(0xFF282A28);
+  static const Color Yellow =Color(0xFFF6BD00);
+  static const Color Dark =Color(0xFF121312);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -77,6 +77,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
+
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
@@ -89,6 +90,10 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
+          children: [
+            CircleAvatar(backgroundImage: AssetImage("assets/images/logo.png"),)
+          ],
+          ),
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
@@ -102,15 +107,8 @@ class _MyHomePageState extends State<MyHomePage> {
           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+
+
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
