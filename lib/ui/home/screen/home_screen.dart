3b50d5aac73edff10a98b8ea0ad49a76540e8b05
Youssef <swimmer.shar12@gmail.com>
2025-09-15 18:33:50 +0300

    + Finish validation for signUp screen

--- lib/ui/home/screen/home_screen.dart
+++ lib/ui/home/screen/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../../signUp/screen/signUp_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String routeName = 'home';
@@ -7,7 +10,24 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Home Screen"),
+      appBar: AppBar(
+        title: const Text("Home"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () async {
+              await FirebaseAuth.instance.signOut();
+              Navigator.pushReplacementNamed(
+                context,
+                SignUpScreen.routeName,
+              );
+            },
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text("Home Screen"),
+      ),
     );
   }
 }
--- lib/ui/signUp/screen/signUp_screen.dart
+++ lib/ui/signUp/screen/signUp_screen.dart
@@ -50,8 +50,9 @@ class _SignUpScreenState extends State<SignUpScreen> {
     }
 
     return Scaffold(
+      backgroundColor: ColorManager.primaryColor,
       appBar: AppBar(
-        title:  Text(
+        title: Text(
           "register".tr(),
           style: TextStyle(
             fontWeight: FontWeight.w400,
@@ -63,246 +64,250 @@ class _SignUpScreenState extends State<SignUpScreen> {
         backgroundColor: ColorManager.primaryColor,
       ),
       body: Container(
-        color: ColorManager.primaryColor,
         padding: const EdgeInsets.all(16),
         child: Form(
-         key: formKey,
-        child: SingleChildScrollView(
-          child: Column(
-          
-            children: [
-              // Avatars
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    radius: 40,
-                    backgroundImage: AssetImage(AssetsManager.avatar1),
-                  ),
-                  const SizedBox(width: 16),
-                  CircleAvatar(
-                    radius: 50,
-                    backgroundImage: AssetImage(AssetsManager.avatar2),
-                  ),
-                  const SizedBox(width: 16),
-                  CircleAvatar(
-                    radius: 40,
-                    backgroundImage: AssetImage(AssetsManager.avatar3),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 10),
-               Text(
-                'avatar'.tr(),
-                style: TextStyle(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 16,
-                  color: ColorManager.white,
+          key: formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                // Avatars
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundImage: AssetImage(AssetsManager.avatar1),
+                    ),
+                    const SizedBox(width: 16),
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundImage: AssetImage(AssetsManager.avatar2),
+                    ),
+                    const SizedBox(width: 16),
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundImage: AssetImage(AssetsManager.avatar3),
+                    ),
+                  ],
                 ),
-              ),
-              const SizedBox(height: 24),
-          
-              // Name
-              CustomField(
-                isPassword: false,
-                hint: 'name'.tr(),
-                keyboard: TextInputType.name,
-                controller: nameController,
-                validation: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Name can't be empty";
-                  }
-                  return null;
-                },
-                prefix: AssetsManager.name,
-              ),
-              const SizedBox(height: 24),
-          
-              // Email
-              CustomField(
-                isPassword: false,
-                hint: 'email'.tr(),
-                keyboard: TextInputType.emailAddress,
-                controller: emailController,
-                validation: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Email can't be empty";
-                  }
-                  if (!RegExp(emailRegex).hasMatch(value)) {
-                    return "Email not valid";
-                  }
-                  return null;
-                },
-                prefix: AssetsManager.email,
-              ),
-              const SizedBox(height: 24),
-          
-              // Password
-              CustomField(
-                isPassword: true,
-                hint: 'password'.tr(),
-                keyboard: TextInputType.visiblePassword,
-                controller: passwordController,
-                validation: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Password can't be empty";
-                  }
-                  if (value.length < 6) {
-                    return "Password must be at least 6 characters";
-                  }
-                  return null;
-                },
-                prefix: AssetsManager.password,
-              ),
-              const SizedBox(height: 24),
-          
-              // Confirm Password
-              CustomField(
-                isPassword: true,
-                hint: 'confirmPassword'.tr(),
-                keyboard: TextInputType.visiblePassword,
-                controller: confirmPasswordController,
-                validation: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Confirm your password";
-                  }
-                  if (value != passwordController.text) {
-                    return "Passwords do not match";
-                  }
-                  return null;
-                },
-                prefix: AssetsManager.password,
-              ),
-              const SizedBox(height: 24),
-          
-              // Phone Number
-              CustomField(
-                isPassword: false,
-                hint: 'phone'.tr(),
-                keyboard: TextInputType.phone,
-                controller: phoneController,
-                validation: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Phone can't be empty";
-                  }
-                  if (!RegExp(r'^[0-9]{11}$').hasMatch(value)) {
-                    return "Phone not valid";
-                  }
-                  return null;
-                },
-                prefix: AssetsManager.phone,
-              ),
-          
-              const SizedBox(height: 32),
-          
-              // Sign Up Button
-              Container(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: () async {
-                    debugPrint("Name: ${nameController.text}");
-                    debugPrint("Email: ${emailController.text}");
-                    debugPrint("Password: ${passwordController.text}");
-                    debugPrint(
-                        "Confirm Password: ${confirmPasswordController.text}");
-                    debugPrint("Phone: ${phoneController.text}");
-                    if (formKey.currentState!.validate()) {
-                      await createAccount();
+                const SizedBox(height: 10),
+                Text(
+                  'avatar'.tr(),
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                    color: ColorManager.white,
+                  ),
+                ),
+                const SizedBox(height: 24),
 
+                // Name
+                CustomField(
+                  isPassword: false,
+                  hint: 'name'.tr(),
+                  keyboard: TextInputType.name,
+                  controller: nameController,
+                  validation: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "nameReq".tr();
                     }
+                    return null;
                   },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: ColorManager.yellow,
-                    padding:
-                    const EdgeInsets.symmetric(horizontal: 50, vertical: 14),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                  ),
-                  child:  Text(
-                    'createAcc'.tr(),
-                    style: TextStyle(
-                      color: ColorManager.primaryColor,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+                  prefix: AssetsManager.name,
                 ),
-              ),
-              SizedBox(height: 17),
-          
-              // have acc
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                   Text(
-                    "haveAcc".tr(),
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
+                const SizedBox(height: 24),
+
+                // Email
+                CustomField(
+                  isPassword: false,
+                  hint: 'email'.tr(),
+                  keyboard: TextInputType.emailAddress,
+                  controller: emailController,
+                  validation: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "emailReq".tr();
+                    }
+                    if (!RegExp(emailRegex).hasMatch(value)) {
+                      return "emailInvalid".tr();
+                    }
+                    return null;
+                  },
+                  prefix: AssetsManager.email,
+                ),
+                const SizedBox(height: 24),
+
+                // Password
+                CustomField(
+                  isPassword: true,
+                  hint: 'password'.tr(),
+                  keyboard: TextInputType.visiblePassword,
+                  controller: passwordController,
+                  validation: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "passReq".tr();
+                    }
+                    if (value.length < 6) {
+                      return "passWeak".tr();
+                    }
+                    return null;
+                  },
+                  prefix: AssetsManager.password,
+                ),
+                const SizedBox(height: 24),
+
+                // Confirm Password
+                CustomField(
+                  isPassword: true,
+                  hint: 'confirmPassword'.tr(),
+                  keyboard: TextInputType.visiblePassword,
+                  controller: confirmPasswordController,
+                  validation: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "confirmReq".tr();
+                    }
+                    if (value != passwordController.text) {
+                      return "passNotMatch".tr();
+                    }
+                    return null;
+                  },
+                  prefix: AssetsManager.password,
+                ),
+                const SizedBox(height: 24),
+
+                // Phone Number
+                CustomField(
+                  isPassword: false,
+                  hint: 'phone'.tr(),
+                  keyboard: TextInputType.phone,
+                  controller: phoneController,
+                  validation: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "phoneReq".tr();
+                    }
+                    if (!RegExp(r'^[0-9]{11}$').hasMatch(value)) {
+                      return "phoneInvalid".tr();
+                    }
+                    return null;
+                  },
+                  prefix: AssetsManager.phone,
+                ),
+
+                const SizedBox(height: 32),
+
+                // Sign Up Button
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      if (formKey.currentState!.validate()) {
+                        await createAccount();
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: ColorManager.yellow,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 50, vertical: 14),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
                     ),
-                  ),
-                  TextButton(
-                    onPressed: () {},
-                    child:  Text(
-                      "login".tr(),
+                    child: Text(
+                      'createAcc'.tr(),
                       style: TextStyle(
-                        color: Colors.yellow,
-                        fontSize: 14,
+                        color: ColorManager.primaryColor,
+                        fontSize: 16,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                ],
-              ),
-          
-              SizedBox(height: 24),
-          
-              /// Language Switch
-              CustomSwitch(
-                onChange: (value) {
-                  setState(() {
-                    selectedLanguage = value;
-                  });
-                  if (selectedLanguage == 1) {
-                    context.setLocale(const Locale("ar"));
-                  } else {
-                    context.setLocale(const Locale("en"));
-                  }
-                },
-                icons: [
-                  SvgPicture.asset(AssetsManager.us, height: 30, width: 30),
-                  SvgPicture.asset(AssetsManager.eg, height: 30, width: 30),
-                ],
-                current: selectedLanguage,
-              ),
-          
-          
-            ],
+                ),
+                const SizedBox(height: 17),
+
+                // have acc
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "haveAcc".tr(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        // TODO: Navigate to Login Screen
+                      },
+                      child: Text(
+                        "login".tr(),
+                        style: TextStyle(
+                          color: Colors.yellow,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 24),
+
+                /// Language Switch
+                CustomSwitch(
+                  onChange: (value) {
+                    setState(() {
+                      selectedLanguage = value;
+                    });
+                    if (selectedLanguage == 1) {
+                      context.setLocale(const Locale("ar"));
+                    } else {
+                      context.setLocale(const Locale("en"));
+                    }
+                  },
+                  icons: [
+                    SvgPicture.asset(AssetsManager.us, height: 30, width: 30),
+                    SvgPicture.asset(AssetsManager.eg, height: 30, width: 30),
+                  ],
+                  current: selectedLanguage,
+                ),
+              ],
+            ),
           ),
         ),
       ),
-    )
     );
   }
-  createAccount()async{
-   try{
-     DialogUtils.showLoadingDialog(context);
-     var credential=await FirebaseAuth.instance.createUserWithEmailAndPassword(
-         email: emailController.text,
-         password: passwordController.text
-     );
-     print(credential.user?.uid);
-     Navigator.pop(context);
-     Navigator.pushNamed(context, HomeScreen.routeName);
 
-   }on FirebaseAuthException catch (e) {
-     Navigator.pop(context);
-     if (e.code == 'weak-password') {
-       print('The password provided is too weak.');
-     } else if (e.code == 'email-already-in-use') {
-       print('The account already exists for that email.');
-     }
-   }
-   }
+  Future<void> createAccount() async {
+    try {
+      DialogUtils.showLoadingDialog(context);
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+
+      Navigator.pop(context); // close loading
+      Navigator.pushReplacementNamed(context, HomeScreen.routeName);
+    } on FirebaseAuthException catch (e) {
+      Navigator.pop(context);
+
+      String message = "";
+      if (e.code == 'weak-password') {
+        message = "weakPassword".tr();
+      } else if (e.code == 'email-already-in-use') {
+        message = "emailInUse".tr();
+      } else if (e.code == 'invalid-email') {
+        message = "invalidEmail".tr();
+      } else {
+        message = "unknownError".tr();
+      }
+
+      DialogUtils.showMessage(
+        context,
+        message,
+        title: "Error",
+        posActionName: "OK",
+      );
+    }
+  }
 }
