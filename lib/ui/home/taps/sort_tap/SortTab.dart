e71ca55cd79d506b1e5d55909d61ffce823f5b10
ahmedhossam-10 <ahmedhossam.4940@gmail.com>
2025-09-17 19:46:34 +0300

    +add NavigationBar in Home screen +But Home tab Not completing yet

---
+++ lib/ui/home/taps/sort_tap/SortTab.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class SortTab extends StatelessWidget {
+  const SortTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.blue,);  }
+}
