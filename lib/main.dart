8f58c98655e93f02783e311830f883a5803704ba
ahmedhossam-10 <ahmedhossam.4940@gmail.com>
2025-09-26 02:08:46 +0300

    +finish google sign in

--- lib/main.dart
+++ lib/main.dart
@@ -32,42 +32,35 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to signUp the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      debugShowCheckedModeBanner: false,
-      initialRoute: SplashScreen.routeName,
-      routes: {
-        SplashScreen.routeName: (_) => SplashScreen(),
-        SignUpScreen.routeName: (_) => SignUpScreen(),
-        LogInScreen.routeName: (_) => LogInScreen(),
-        HomeScreen.routeName: (_) => HomeScreen(),
-        HomeTab.routeName: (_) => HomeTab(),
-        SortTab.routeName: (_) => SortTab(),
-        SearchTab.routeName: (_) => SearchTab(),
+    return ScreenUtilInit(
+      designSize: const Size(390, 844),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          ),
+          debugShowCheckedModeBanner: false,
+          initialRoute: SplashScreen.routeName,
+          routes: {
+            SplashScreen.routeName: (_) => SplashScreen(),
+            OnBoardingPage.routeName: (_) => OnBoardingPage(),
+            SignUpScreen.routeName: (_) => SignUpScreen(),
+            LogInScreen.routeName: (_) => LogInScreen(),
+            HomeScreen.routeName: (_) => HomeScreen(),
+            MovieDetailsScreen.routeName: (context) {
+              final movieId = ModalRoute.of(context)!.settings.arguments as int;
+              return MovieDetailsScreen(movieId: movieId);
+            },
+          },
+        );
       },
     );
   }
